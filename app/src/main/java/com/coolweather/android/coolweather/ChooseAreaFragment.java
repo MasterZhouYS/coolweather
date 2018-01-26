9e5baf47478f9718235501dc645b979ce177a9f4
zhou <1838448569@qq.com>
2018-01-26 16:54:20 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/coolweather/android/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,7 @@
+package com.coolweather.android.coolweather;
+
+/**
+ * Created by Administrator on 2018/1/26 0026.
+ */
+public class ChooseAreaFragment {
+}
---
+++ app/src/main/java/com/coolweather/android/util/LogUtil.java
@@ -0,0 +1,7 @@
+package com.coolweather.android.util;
+
+/**
+ * Created by Administrator on 2018/1/26 0026.
+ */
+public class LogUtil {
+}
